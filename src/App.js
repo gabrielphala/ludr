e13ebeada11d8c7156650cba1b471c5928c040d6
Gabriel Phala <phalagee7@gmail.com>
2022-07-28 00:22:38 +0200

    Added handlebar helpers Changed start-up log info

--- src/App.js
+++ src/App.js
@@ -7,15 +7,31 @@ import Middleware from "./Middleware";
 export const Load = () => {
     // Log info
     if (Config.showInfo) {
-        console.log(`
-            base views: ${Config.baseViews}
-            component views: ${Config.componentViews}
-            layout views: ${Config.layoutViews}
-        `);
+        console.log(
+            `base views: ${Config.baseViews}` +
+            `\ncomponent views: ${Config.componentViews}` +
+            `\nlayout views: ${Config.layoutViews}`
+        );
     }
 
     Env.handlebars.registerHelper('component', function (str) {
         return `@ludr_component ${str};`;
+    })
+
+    Env.handlebars.registerHelper('globalLinkActive', function (str) {
+        const linkActiveClassArr = str.split('data:');
+
+        return linkActiveClassArr.length == 1 ?
+            `ludr_link_active_class ${str};` : 
+            `ludr_link_active_class ${Env.get(linkActiveClassArr[1])};`
+    })
+
+    Env.handlebars.registerHelper('link', function (str) {
+        return `data-linkaddress="${str}"`;
+    })
+
+    Env.handlebars.registerHelper('linkActive', function (str) {
+        return `data-linkactive="${str}"`;
     })
 
     const route = Router.getRoute();
