2adabf065cb6e3ff685e847dd2b6f150db6dd511
Gabriel Phala <phalagee7@gmail.com>
2022-07-28 17:12:26 +0200

    added caching

--- src/Components/Component.js
+++ src/Components/Component.js
@@ -10,9 +10,28 @@ class Component {
         this.scope = scope;
         this.linkActiveClass = '';
 
+        this._rawContent = '';
+        this._content = '';
+
         path = Utils.hasExt(path) ? path : `${path}.${Config.viewsExt}`;
 
         this.path = Url.cleanURL(`${Config.componentViews}/${path}`);
+    }
+
+    get rawContent () {
+        return this._rawContent;
+    }
+
+    set rawContent (rawContent) {
+        this._rawContent = rawContent;
+    }
+
+    get content () {
+        return this._content;
+    }
+
+    set content (content) {
+        this._content = content;
     }
 };
 
