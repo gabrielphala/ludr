9f56bc7e36dbed346a86c80e411d2801945ea81a
Gabriel Phala <phalagee7@gmail.com>
2022-07-28 17:11:57 +0200

    added once off middleware

--- src/Middleware.js
+++ src/Middleware.js
@@ -18,6 +18,14 @@ export default new (class Middleware {
     }
 
     add (scope, callback) {
+        this.addMiddleware(scope, callback)
+    }
+
+    once (scope, callback) {
+        this.addMiddleware(scope, callback, true)
+    }
+
+    addMiddleware (scope, callback, once = false) {
         let _scope = scope;
 
         scope = !callback ? null : scope;
@@ -27,6 +35,8 @@ export default new (class Middleware {
 
         this.middleware.push({
             scope,
+            once,
+            hasRun: false,
             callback
         });
     };
@@ -36,25 +46,44 @@ export default new (class Middleware {
     }
 
     reset () {
+        let newMiddleware = [];
+
         this.middlewareIndex = 0;
         this.readyMiddleware = [];
+
+        this.middleware.forEach(middleware => {
+            if (middleware.hasRun)
+                return;
+
+            newMiddleware.push(middleware);
+        });
+
+        this.middleware = newMiddleware;
     }
 
     exec () {
-        const { callback } = this.readyMiddleware[this.index] || { callback: 'done' };
+        let index = this.middlewareIndex;
+
+        const { callback, once, hasRun } = this.readyMiddleware[this.index] || { callback: 'done' };
 
         if (typeof callback != 'function')
             return;
 
+        if (once && hasRun)
+            return this.exec();
+
+        else if (once && !hasRun)
+            this.readyMiddleware[index].hasRun = true;
+        
         callback(() => this.exec());
     }
 
     run () {
         this.reset();
 
-        this.middleware.forEach(({ scope, callback }) => {
-            if (!(scope[0]) || (scope.includes(Router.currentRoute.name)))
-                this.readyMiddleware.push({scope, callback});
+        this.middleware.forEach((middleware) => {
+            if (!(middleware.scope[0]) || (middleware.scope.includes(Router.currentRoute.name)))
+                this.readyMiddleware.push(middleware);
         });
 
         this.exec();
