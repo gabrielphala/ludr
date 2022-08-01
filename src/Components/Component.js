c32ded065eb0fab9a3317dc56c89bc0b6ca97548
Gabriel Phala <phalagee7@gmail.com>
2022-08-01 13:26:06 +0200

    removed rawContent

--- src/Components/Component.js
+++ src/Components/Component.js
@@ -4,26 +4,17 @@ import Url from "../Url";
 import Utils from "../Utils";
 
 class Component {
-    constructor(name, { path, nav, scope, data } = null) {
+    constructor (name, { path, nav, scope, data } = null) {
         this.name = name;
         this.nav = nav;
         this.scope = scope;
         this.linkActiveClass = '';
 
-        this._rawContent = '';
         this._content = '';
 
         path = Utils.hasExt(path) ? path : `${path}.${Config.viewsExt}`;
 
         this.path = Url.cleanURL(`${Config.componentViews}/${path}`);
-    }
-
-    get rawContent () {
-        return this._rawContent;
-    }
-
-    set rawContent (rawContent) {
-        this._rawContent = rawContent;
     }
 
     get content () {
