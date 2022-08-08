3985858d53bf6aac0f36433526cdb7b9d567e682
Gabriel Phala <phalagee7@gmail.com>
2022-08-09 00:06:14 +0200

    Added JSDocs implemented DOM event functionality

--- src/Layouts/index.js
+++ src/Layouts/index.js
@@ -1,8 +1,14 @@
 import Middleware from "../Middleware";
 import Components from "../Components";
 import Utils from "../Utils";
+import Events from "../Events";
+import Router from "../Router";
 
 export default new (class Layouts {
+    /**
+     * @date 2022-08-08
+     * @return {Layouts}
+     */
     constructor () {
         if (Layouts.instance)
             return Layouts.instance;
@@ -12,10 +18,23 @@ export default new (class Layouts {
         Layouts.instance = this;
     }
 
+    /**
+     * Add new layout to the cache and returns it
+     * @date 2022-08-08
+     * @param {string} name
+     * @param {object} layout
+     * @return {object}
+     */
     add (name, layout) {
-        this.layouts[name] = layout;
+        return this.layouts[name] = layout;
     }
 
+    /**
+     * Returns a layout
+     * @date 2022-08-08
+     * @param {string} name
+     * @return {object}
+     */
     use (name) {
         if (!this.layouts[name])
             throw `No corresponding layout: there is no pre-defined layout with the name: (${name})`;
@@ -23,6 +42,12 @@ export default new (class Layouts {
         return this.layouts[name];
     }
 
+    /**
+     * Populates layout with components' content
+     * and prepends layout content to the document body 
+     * @date 2022-08-08
+     * @param {name} name
+     */
     build (name) {
         if (!this.layouts[name])
             throw `No corresponding layout: there is no pre-defined layout with the name: (${name})`;
@@ -32,11 +57,22 @@ export default new (class Layouts {
         Middleware.once(() => {
             Utils.prependToBody(this.layouts[name].content)
 
-            Components.initHighlightNavItems();
             Components.initNavEvents();
+
+            Components.initHighlightNavItems();
+
+            const router = Router.use(Router.currentRoute.name);
+
+            (new Events).setEventListeners(router.blueprint);
         })
     }
 
+    /**
+     * Removes unused elements and adss new elements
+     * @date 2022-08-08
+     * @param {object} oldRoute
+     * @param {object} currentRoute
+     */
     switch (oldRoute, currentRoute) {
         const layout = this.layouts[currentRoute.layout];
 
@@ -49,6 +85,8 @@ export default new (class Layouts {
         Middleware.once((next) => {
             layout.removeUnusedElements(oldRoute.blueprint, currentRoute.blueprint);
             layout.addNewElements(oldRoute.blueprint, currentRoute.blueprint);
+
+            (new Events).setEventListeners(currentRoute.blueprint);
 
             next()
         })
