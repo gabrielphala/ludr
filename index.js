c75f6eb2f9a72c38b8acacc58dfb679d9e510edb
Gabriel Phala <phalagee7@gmail.com>
2022-10-24 21:22:11 +0200

    exposed Components, Store, and Reload

--- index.js
+++ index.js
@@ -1,38 +1,47 @@
 import Layout from "./src/Layouts/Layout";
 import Component from "./src/Components/Component";
+import Components from "./src/Components";
 import Group from "./src/Groups/Group";
 import Router from "./src/Router";
 import Route from "./src/Router/Route";
 import Singleton from "./src/Singleton";
+import Store from "./src/Store";
 import Events from "./src/Events";
 import Middleware from "./src/Middleware";
 import Config from "./src/Config";
 
-import { Load, Next } from "./src/App";
+import { Load, Next, Reload } from "./src/App";
 
 export {
     Layout,
     Component,
+    Components,
     Group,
     Router,
     Route,
     Singleton,
+    Store,
     Events,
     Middleware,
     Config,
     Load,
-    Next
+    Next,
+    Reload
 }
 
 export default {
     Layout,
     Component,
+    Components,
     Group,
     Router,
     Route,
     Singleton,
+    Store,
     Events,
     Middleware,
     Config,
-    Load
+    Load,
+    Next,
+    Reload
 }
